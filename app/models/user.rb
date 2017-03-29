8c1313e92851280a14b38c89243ae9a38eebf609
John P <john.pickett@gmail.com>
2017-03-29 16:49:24 -0500

    ckp-1 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
+
+  after_create :send_confirm_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  private
+
+  def send_confirm_email
+    UserMailer.new_user(self).deliver_now
+  end
+
 end
