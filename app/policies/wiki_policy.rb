31c6141910ef0652d37b704b3310c1a561b7e44d
John P <john.pickett@gmail.com>
2017-05-03 15:18:17 -0500

    fixed wiki index view

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -9,13 +9,15 @@ class WikiPolicy < ApplicationPolicy
 
     def resolve
       wikis = []
-      if user.admin?
-        wikis = scope.all # if the user is an admin, show them all the wikis
-      elsif user.premium?
-        all_wikis = scope.all
-        all_wikis.each do |wiki|
-          if wiki.public? || wiki.user == user || wiki.users.include?(user)
-            wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
+      if user
+        if user.admin?
+          wikis = scope.all # if the user is an admin, show them all the wikis
+        elsif user.premium?
+          all_wikis = scope.all
+          all_wikis.each do |wiki|
+            if wiki.public? || wiki.user == user || wiki.users.include?(user)
+              wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
+            end
           end
         end
       else # this is the lowly standard user
