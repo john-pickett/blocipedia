07a1fafbe4ad66d4999d9acfd4f82a37d527d3fc
John P <john.pickett@gmail.com>
2017-05-03 16:45:12 -0500

    working on wiki index and roles

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -17,19 +17,31 @@ class WikiPolicy < ApplicationPolicy
           all_wikis.each do |wiki|
             if wiki.public? || wiki.user == user || wiki.users.include?(user)
               wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
-            end
-          end
-        end
+            end #end of all_wikis loop
+          end #end of elsif
       else # this is the lowly standard user
         all_wikis = scope.all
         wikis = []
         all_wikis.each do |wiki|
           if wiki.public? || wiki.users.include?(user)
             wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
-          end
-        end
+          end #end of all_wikis loop
+
+        end #end of if user
       end
       wikis # return the wikis array we've built up
+    else # in no current_user
+      all_wikis = scope.all
+      wikis = []
+      all_wikis.each do |wiki|
+        if wiki.public? || wiki.users.include?(user)
+          wikis << wiki # only show standard users public wikis and private wikis they are a collaborator on
+        end #end of all_wikis loop
+
+      end #end of if user
     end
+
+
   end
+end
 end
